0d5a0e7efb7217aa99ff83df3ed1b7926b3dc2ce
mikey <mikey@hey.com>
2024-04-14 18:44:55 -0500

    users can see single reviews

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ mongoose.connect('mongodb://localhost/rotten-potatoes', {
 const Review = mongoose.model('Review', {
   title: String,
   description: String,
-  movieTitle: String
+  movieTitle: String,
 });
 
 // body-parser ----------------------------------
@@ -71,7 +71,18 @@ app.post('/reviews', (req, res) => {
   Review.create(req.body)
     .then((review) => {
       console.log(review);
-      res.redirect('/');
+      res.redirect(`/reviews/${review._id}`); // Redirect to reviews/:id
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id)
+    .then((review) => {
+      res.render('reviews-show', { review: review });
     })
     .catch((err) => {
       console.log(err.message);
