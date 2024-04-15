2d6845f0bf5dab7b6eec06acf7946833371a91e6
mikey <mikey@hey.com>
2024-04-14 19:13:30 -0500

    add controllers and models folders for separation of concerns

--- app.js
+++ app.js
@@ -31,12 +31,6 @@ mongoose.connect('mongodb://localhost/rotten-potatoes', {
   useNewUrlParser: true,
 });
 
-const Review = mongoose.model('Review', {
-  title: String,
-  description: String,
-  movieTitle: String,
-});
-
 // body-parser ----------------------------------
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -53,87 +47,8 @@ app.use(methodOverride('_method'));
 // ];
 
 // routes ----------------------------------
-// INDEX
-app.get('/', (req, res) => {
-  Review.find()
-    .then((reviews) => {
-      // executed when the promise resolves
-      res.render('reviews-index', { reviews: reviews });
-    })
-    .catch((err) => {
-      // executed if the promise is rejected
-      console.log(err);
-    });
-});
-
-// NEW
-app.get('/reviews/new', (req, res) => {
-  res.render('reviews-new', { title: 'New Review' });
-});
-
-// CREATE
-app.post('/reviews', (req, res) => {
-  Review.create(req.body)
-    .then((review) => {
-      console.log(review);
-      res.redirect(`/reviews/${review._id}`); // Redirect to reviews/:id
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-});
-
-// SHOW
-app.get('/reviews/:id', (req, res) => {
-  Review.findById(req.params.id)
-    .then((review) => {
-      res.render('reviews-show', { review: review });
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-});
-
-// EDIT
-app.get('/reviews/:id/edit', (req, res) => {
-  Review.findById(req.params.id)
-    .then((review) => {
-      res.render('reviews-edit', {
-        review: review,
-        title: 'Edit Review',
-      });
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error retrieving review');
-    });
-});
-
-// UPDATE
-app.put('/reviews/:id', (req, res) => {
-  Review.findByIdAndUpdate(req.params.id, req.body)
-    .then((review) => {
-      res.redirect(`/reviews/${review._id}`);
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-});
-
-// DELETE
-app.delete('/reviews/:id', function (req, res) {
-  Review.findOneAndDelete({ _id: req.params.id })
-    .then((review) => {
-      if (!review) {
-        return res.status(404).send('Review not found');
-      }
-      res.redirect('/');
-    })
-    .catch((err) => {
-      console.log(err.message);
-      res.status(500).send('Error deleting review');
-    });
-});
+// refactored into controllers
+const reviews = require('./controllers/reviews')(app);
 
 // server ----------------------------------
 app.listen(3000, () => {
---
+++ controllers/reviews.js
@@ -0,0 +1,84 @@
+const Review = require('../models/review');
+module.exports = function (app) {
+  // INDEX
+  app.get('/', (req, res) => {
+    Review.find()
+      .then((reviews) => {
+        // executed when the promise resolves
+        res.render('reviews-index', { reviews: reviews });
+      })
+      .catch((err) => {
+        // executed if the promise is rejected
+        console.log(err);
+      });
+  });
+
+  // NEW
+  app.get('/reviews/new', (req, res) => {
+    res.render('reviews-new', { title: 'New Review' });
+  });
+
+  // CREATE
+  app.post('/reviews', (req, res) => {
+    Review.create(req.body)
+      .then((review) => {
+        console.log(review);
+        res.redirect(`/reviews/${review._id}`); // Redirect to reviews/:id
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
+
+  // SHOW
+  app.get('/reviews/:id', (req, res) => {
+    Review.findById(req.params.id)
+      .then((review) => {
+        res.render('reviews-show', { review: review });
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
+
+  // EDIT
+  app.get('/reviews/:id/edit', (req, res) => {
+    Review.findById(req.params.id)
+      .then((review) => {
+        res.render('reviews-edit', {
+          review: review,
+          title: 'Edit Review',
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error retrieving review');
+      });
+  });
+
+  // UPDATE
+  app.put('/reviews/:id', (req, res) => {
+    Review.findByIdAndUpdate(req.params.id, req.body)
+      .then((review) => {
+        res.redirect(`/reviews/${review._id}`);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
+
+  // DELETE
+  app.delete('/reviews/:id', function (req, res) {
+    Review.findOneAndDelete({ _id: req.params.id })
+      .then((review) => {
+        if (!review) {
+          return res.status(404).send('Review not found');
+        }
+        res.redirect('/');
+      })
+      .catch((err) => {
+        console.log(err.message);
+        res.status(500).send('Error deleting review');
+      });
+  });
+};
