227bd6819baccc328727378f1985dcdc36f10a36
mikey <mikey@hey.com>
2024-04-20 15:00:33 -0500

    users can see movie pages and trailers

--- controllers/movies.js
+++ controllers/movies.js
@@ -5,11 +5,34 @@ module.exports = function (app) {
   // const moviedb = new MovieDb('0d3489c55fa0efcc4b91979110fe67cc');
   const moviedb = new MovieDb(process.env.MOVIEDB_API_KEY);
 
+  // HOME
   app.get('/', (req, res) => {
     moviedb
       .movieNowPlaying()
       .then((response) => {
         res.render('movies-index', { movies: response.results });
+      })
+      .catch(console.error);
+  });
+
+  // SHOW
+  app.get('/movies/:id', (req, res) => {
+    moviedb
+      .movieInfo({ id: req.params.id })
+      .then((movie) => {
+        moviedb
+          .movieVideos({ id: req.params.id })
+          .then((videos) => {
+            const trailer = videos.results.find(
+              (video) => video.type === 'Trailer'
+            );
+            if (trailer) {
+              movie.trailer_youtube_id = trailer.key;
+            }
+
+            res.render('movies-show', { movie: movie });
+          })
+          .catch(console.error);
       })
       .catch(console.error);
   });
